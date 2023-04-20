def632a808467ca520a0f99650f6d3ddfcd03ed9
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-04-20 23:32:26 +0800

    feat:完成了商品列表的开发

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
-
+import * as filters from '@/filters/index.js'
 Vue.config.productionTip = false
+
+Object.keys(filters).forEach(key => Vue.filter(key, filters[key]))
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- subGoods/goods-list/goods-list.vue
+++ subGoods/goods-list/goods-list.vue
@@ -1,6 +1,11 @@
-<template>
-	<view>
-		list
+<template class="listMain">
+	<view class="card">
+		<view class="card">
+			<van-card v-for="item in goodsList" :key="item.goods_id" :price="item.goods_price | toFixed"
+				:title="item.goods_name" :thumb="item.goods_big_logo || defaultImg"
+				:thumb-link="'/subGoods/goods-detail/goods-detail?id=' + item.goods_id" />
+			<van-divider v-if="isFinished" contentPosition="center">我是有底线的</van-divider>
+		</view>
 	</view>
 </template>
 
@@ -27,7 +32,9 @@
 				// 总数量，用来实现分页
 				total: 0,
 				isLoading: false,
-				isFinished: false
+				isFinished: false,
+				defaultImg: "https://img01.yzcdn.cn/vant/ipad.jpeg",
+
 			};
 		},
 		onLoad({
@@ -44,13 +51,43 @@
 				const {
 					message
 				} = await getGoodsListAPI(this.queryObj)
+				this.isLoading = false
 				console.log(message);
 				this.total = message.total
+				this.goodsList.push(...message.goods)
+				// 加载完成显示分割线
+				if (this.goodsList.length >= this.total) this.isFinished = true
 			}
+		},
+		// 上滑加载更多
+		onReachBottom() {
+			if (this.isLoading || this.isFinished) return
+			this.queryObj.pagenum++
+			this.loadGoodsList()
+		},
+		// 下拉刷新
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isLoading = false
+			this.goodsList = []
+			this.loadGoodsList()
+			uni.stopPullDownRefresh()
 		}
 	}
 </script>
 
 <style lang="scss">
+	.listMain {
+		padding: 20rpx;
+		background-color: #eee;
 
+		.custom-class {
+			width: 700rpx;
+			margin: 10rpx 10rpx 0;
+			border-radius: 24rpx;
+			background-color: red;
+			box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.2);
+		}
+	}
 </style>
